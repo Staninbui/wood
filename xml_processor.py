39cc0264ac32a55b0137252f2b3a93621f682f07
stan <341451903@qq.com>
2025-09-05 23:09:44 +0900

    simplify SSL bypass in xml_processor with environment variables and unverified context

--- xml_processor.py
+++ xml_processor.py
@@ -7,22 +7,22 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import logging
 from typing import List, Dict, Optional
 import time
+
+# Set environment variables to disable SSL verification
+os.environ['PYTHONHTTPSVERIFY'] = '0'
+os.environ['CURL_CA_BUNDLE'] = ''
+os.environ['REQUESTS_CA_BUNDLE'] = ''
+
+# Import SSL modules after setting environment variables
 import ssl
 import urllib3
-from urllib3.util import ssl_
 
-# SSL configuration to prevent recursion errors (same as main app)
-ssl._create_default_https_context = ssl._create_unverified_context
+# Disable all SSL warnings
+urllib3.disable_warnings()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Monkey patch urllib3 to avoid SSL context creation issues
-def patched_create_urllib3_context(ssl_version=None, cert_reqs=None, options=None, ciphers=None):
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
-    return context
-
-ssl_.create_urllib3_context = patched_create_urllib3_context
+# Completely bypass SSL verification
+ssl._create_default_https_context = ssl._create_unverified_context
 
 logger = logging.getLogger(__name__)
 
@@ -35,22 +35,9 @@ class XMLProcessor:
         self.session = self._create_ssl_session()
     
     def _create_ssl_session(self):
-        """Create SSL-safe session to prevent recursion errors"""
+        """Create simple session with SSL verification disabled"""
         session = requests.Session()
         session.verify = False
-        
-        # Use a custom adapter to bypass SSL issues
-        from requests.adapters import HTTPAdapter
-        
-        class NoSSLAdapter(HTTPAdapter):
-            def init_poolmanager(self, *args, **kwargs):
-                kwargs['ssl_context'] = ssl.create_default_context()
-                kwargs['ssl_context'].check_hostname = False
-                kwargs['ssl_context'].verify_mode = ssl.CERT_NONE
-                return super().init_poolmanager(*args, **kwargs)
-        
-        session.mount('https://', NoSSLAdapter())
-        session.mount('http://', NoSSLAdapter())
         return session
     
     def extract_item_ids_from_zip(self, zip_content: bytes) -> List[str]:
